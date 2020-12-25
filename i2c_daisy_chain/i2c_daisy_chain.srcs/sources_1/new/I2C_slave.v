b4078beceb605e7bc069293d8d3720bd8244ab12
dhruvshah0208 <dhruv0208@gmail.com>
2020-12-25 19:41:53 +0530

    Incorporated Changes

--- i2c_daisy_chain/i2c_daisy_chain.srcs/sources_1/new/I2C_slave.v
+++ i2c_daisy_chain/i2c_daisy_chain.srcs/sources_1/new/I2C_slave.v
@@ -21,137 +21,197 @@
 
 
 module I2C_slave
-#(parameter Address = 7'b1100010)      // Change this later
+#(parameter Address = 7'b1100010)      // Change this later # SLAVE ADDRESS
 (
 inout SDA,
 input SCL,
 output [7:0] data_out,
-output [1:0] control_signals,
+output [1:0] control_first_block,
 input [1:0] control_last_block, // #REVISIT - RB will send 1 if they have acknowledged
 input  [7:0] data_in,
 output [7:0] address,
-output clk
+output clk,
+input resetn // Active Low reset
 );
 wire tick;
-reg reset;
+reg internal_reset;
 wire [7:0] PO; // Parallel Output
 reg enable_piso = 0;
-reg [2:0] state = IDLE;    // STATES - INIT,WRITE_1,WRITE_2,READ,IDLE
+reg [2:0] c_state;    // STATES - INIT,WRITE_1,WRITE_2,READ,IDLE
+reg [2:0] n_state;    // STATES - INIT,WRITE_1,WRITE_2,READ,IDLE
 reg opcode;
-reg [6:0] address_reg;
 reg send,send_ready;
 reg [7:0] data_reg;
 reg [1:0] control_reg;
-reg i2c_slave_ack = 0,i2c_reg_ack = 0;
-reg [6:0] address_next;
+reg i2c_slave_ack ,i2c_reg_ack;
+reg [6:0] address_reg_next;
+reg [6:0] address_reg_current;
+reg c_start,n_start,c_stop,n_stop;
 // parameters
 parameter read = 1'b1;  
 parameter write = 1'b0;
 parameter alternate = 1'b1;
 parameter burst = 1'b0;
 
-posedge_counter DUT(.SCL(SCL),.tick(tick),.reset(reset));  
-serial_input_parallel_output DUT1(.SCL(SCL),.tick(tick),.reset(reset),.PO(PO),.SDA(SDA));  
+posedge_counter DUT(.SCL(SCL),.tick(tick),.reset(internal_reset));  
+serial_input_parallel_output DUT1(.SCL(SCL),.tick(tick),.reset(internal_reset),.PO(PO),.SDA(SDA));  
 parallel_input_serial_output DUT2(.data_in(data_in),.enable(enable_piso),.SCL(SCL),.tick(tick),.send_ready(send_ready),.serial_output(send));
 
 // Define the States 
 parameter INIT = 3'b000;
 parameter READ = 3'b001;
-parameter WRITE_1 = 3'b010;   // Addr
-parameter WRITE_2 = 3'b011;   // Data
-parameter IDLE= 3'b100;   // Data
-
-
-
-// CHECK FOR START AND STOP CONDITIONS
-always @(posedge SDA) begin  // STOP condition
-    if (SCL == 1'b1) begin    
-        state <= IDLE;  
-        reset <= 1;
-        enable_piso <= 0;
-    end
-end 
-always @(negedge SDA) begin      // START CONDITION
-    if (SCL == 1'b1) begin
-        state <= INIT;
-        reset <= 1;
-        enable_piso <= 0;
+parameter WRITE_1 = 3'b011;   // Addr
+parameter WRITE_2 = 3'b100;   // Data
+parameter IDLE= 3'b101;   // Data
+// Initialize Values
+always @(posedge tick or negedge resetn) begin  // State transitions have to occur at posedge of tick
+    if (!resetn) begin // Reset and Initialize all values of reg here
+        c_state <= IDLE;
+        address_reg_current <= 8'h00; // Random Initial Value - CHange Later
+    end else begin
+        c_state <= n_state;
+        address_reg_current <= address_reg_next;
+        
     end
 end
+always @(posedge SDA)// STOP condition
+    if (SCL == 1'b1) 
+        c_stop <= 1;
+    
+always @(negedge SDA)       // START CONDITION
+    if (SCL == 1'b1) 
+        c_start <= 1;
 
-always @(posedge tick) begin
-    address_reg = address_next;   // This is the only blocking assignment in the block... is it fine? #REVISIT
-    case (state) 
-        INIT: begin               // Resetting Tasks have been done by the START/STOP checkers
-            if (PO[7:1] == Address) begin   // MSB is sent first through i2c
-                // SEND ACK  
-                i2c_slave_ack <= 1;
-            end    
-            // Next State Logic
-            if (PO[0] == read) state <= READ;
-            else state <= WRITE_1;
-        end
-        
-        WRITE_1: begin
-            address_reg <= PO[7:1];
-            address_next<= PO[7:1];
-            opcode <= PO[0];
-            control_reg <= {write,1'b0};
-            i2c_reg_ack <= 1;
-            // Next State Logic
-            state <= WRITE_2;    
-        end
-        
-        WRITE_2: begin
-            // Send Address as well as Data this time {Similair to WRITE_1} , ACK - 0, Write Mode
-            // Make arrangements to send appropriate NACK  #REVISIT
-            control_reg <= {write,1'b0};
-            i2c_reg_ack <= 1;
-            data_reg <= PO[7:0];
-            // Next State Logic
-            if (opcode == alternate) state <= WRITE_1;
-            else if (opcode == burst) begin
-                state <= WRITE_2;
-                address_next <= address_reg + 1; // First register will be skipped
+// Tasks of each state
+always @(posedge SCL) begin
+    c_start <= n_start;
+    c_stop <= n_stop;
+    case(c_state) 
+        INIT: begin
+            enable_piso <= 0;
+            if (tick) begin
+                if (PO[7:1] == Address) begin   // MSB is sent first through i2c
+                    // SEND ACK  
+                    i2c_slave_ack <= 1;
+                end  
+            end else begin
+                if (i2c_slave_ack) begin 
+                    send_ready <= 1;
+                    send <= 1'b1;
+                    i2c_slave_ack <= 0;
+                end
             end
         end
-        
-        READ: begin
-            // Send a read command to registers, increment address by one, stay in this state till start/stop  
-            // ENTER CODE HERE - read command to registers on the address stored.Send this at posedge,collect at negedge and send on i2c at next posedge #REVISIT
-            control_reg <= {read,1'bx};
-            enable_piso <= 1;
-            address_next <= address_reg + 1;   // #REVISIT - Problem here... we will skip the first register 
-        end 
+        WRITE_1:begin
     
+            enable_piso <= 0;
+            if (tick) begin 
+                address_reg_next <= PO[7:1];
+                opcode <= PO[0];
+                i2c_reg_ack <= 1;
+                control_reg <= {write,1'b0};
+            end else begin
+                if (i2c_reg_ack) begin 
+                    send <= ~control_last_block[0];  // #REVISIT - Check the sign
+                    send_ready <= 1;    
+                    i2c_reg_ack <= 0;
+                end
+            end         
+         end
+         WRITE_2:begin
+           enable_piso <= 0;
+           if (tick) begin
+                i2c_reg_ack <= 1;
+                control_reg <= {write,1'b0};
+                data_reg <= PO[7:0];   
+            end else begin
+                    if (i2c_reg_ack) begin 
+                        send <= ~control_last_block[0];  // #REVISIT - Check the sign
+                        send_ready <= 1;    
+                        i2c_reg_ack <= 0;
+                    end
+                end 
+          end
+          READ:
+            if(tick) begin
+                control_reg <= {read,1'bx};
+                enable_piso <= 1;       
+            end                      
     endcase
 end
-always @(negedge SCL) begin
-    //#REVISIT
-    reset <= 0;             // Reset can only be turned on when SCL is high 
-    if (tick == 1) begin
-        if (i2c_slave_ack == 1) begin
-            send <= 0;
-            send_ready <= 1;
-        end    
-        if (i2c_reg_ack == 1) begin
-            send <= ~control_last_block[0];
-            send_ready <= 1;    
+
+always @(*) begin  // NEXT STATE COMBO LOGIC
+    case (c_state)
+        IDLE:begin
+            if(c_start) 
+                n_state <= INIT;
         end
-    end 
-    else begin
-        send_ready <= 0;
-        i2c_reg_ack <= 0;
-        i2c_slave_ack <= 0;
-    end 
+        INIT: begin
+            if (PO[0] == read) n_state <= READ;
+            else n_state <= WRITE_1;
+            if (c_start) begin
+                n_state <= INIT;
+                n_start <= 0;
+                // Reset counters         #REVISIT
+            end
+            if (c_stop) begin
+                n_state <= IDLE;
+                n_stop <= 0;
+            end
+        end
+        WRITE_1:begin
+            n_state <= WRITE_2;
+            if (c_start) begin
+                    n_state <= INIT;
+                    n_start <= 0;
+                    // Reset counters         #REVISIT
+            end
+            if (c_stop) begin
+                n_state <= IDLE;
+                n_stop <= 0;
+            end 
+         end
+        WRITE_2: begin
+            if (opcode == alternate) n_state <= WRITE_1;
+            else if (opcode == burst) begin
+                n_state <= WRITE_2;
+                address_reg_next <= address_reg_current + 1; // First register will be skipped
+            end 
+            if (c_start) begin
+                    n_state <= INIT;
+                    n_start <= 0;
+                    // Reset counters         #REVISIT
+            end
+            if (c_stop) begin
+                n_state <= IDLE;
+                n_stop <= 0;
+            end 
+            
+        end
+        READ: begin
+            n_state <= READ;
+            address_reg_next <= address_reg_current + 1;
+            if (c_start) begin
+                    n_state <= INIT;
+                    n_start <= 0;
+                    // Reset counters         #REVISIT
+            end
+            if (c_stop) begin
+                n_state <= IDLE;
+                n_stop <= 0;
+            end 
+            
+        end
+endcase
+    
+
 end
 
-
-assign SDA = (send_ready == 1) ? send:SDA;       // #RECONFIRM
+assign SDA = (send_ready == 1) ? send:SDA;       // #RECONFIRM    What to send when i dont want to control the line?
 assign clk = tick;
-assign address = (tick == 1) ? address_reg:'bx;
-assign control_signals = (tick == 1) ? control_reg:'bx;
-assign data_out = (tick == 1) ? data_reg:'bx;
+assign address = (tick == 1) ? address_reg_current:8'h00;      // Garbage Value
+assign control_first_block = (tick == 1) ? control_reg:8'h00;  // Garbage Value 
+assign data_out = (tick == 1) ? data_reg:8'h00;                // Garbage Value 
 
 endmodule
 
